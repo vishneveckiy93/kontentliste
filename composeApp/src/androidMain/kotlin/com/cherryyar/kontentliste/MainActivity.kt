07d9df235c6c783e70d0de0d6d87b6dda1810cff
Iaroslav Vishnevetskii <iaroslav.vishnevetskii@timelysoft.net>
2025-09-27 11:54:47 +0200

    android: resolve deps and build VM in Activity; pass VM to App()

--- composeApp/src/androidMain/kotlin/com/cherryyar/kontentliste/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/cherryyar/kontentliste/MainActivity.kt
@@ -7,21 +7,32 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import app.App
+import app.di.AppDI
 import core.di.Di
+import feature.accounts.domain.AccountsRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import org.koin.java.KoinJavaComponent.getKoin
 
 class MainActivity : ComponentActivity() {
+    private val activityScope = CoroutineScope(SupervisorJob() + Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-        Di.init()
+        AppDI.init()
+
+        val repo: AccountsRepository = getKoin().get()
+        val vm = AccountsViewModel(repo, acitivityScope)
         setContent {
-            App()
+            App(vm)
         }
     }
-}
 
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
+    override fun onDestroy() {
+        super.onDestroy()
+        activityScope.cancel()
+    }
 }
