998425450b8098b315f0aa1037bff1d68eaf2af8
Iaroslav Vishnevetskii <iaroslav.vishnevetskii@timelysoft.net>
2025-09-27 14:49:32 +0200

    refactor(vm): common ViewModel + internal Koin injection; no Activity scope

--- composeApp/src/commonMain/kotlin/app/App.kt
+++ composeApp/src/commonMain/kotlin/app/App.kt
@@ -2,6 +2,7 @@ package app
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.cherryyar.kontentliste.Greeting
 import feature.accounts.presentation.AccountsEvent
 import feature.accounts.presentation.AccountsViewModel
@@ -11,5 +12,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Composable
 @Preview
 fun App() {
-    Greeting()
+    val vm: AccountsViewModel = viewModel { AccountsViewModel() }
+    LaunchedEffect(Unit) { vm.onEvent(AccountsEvent.Refresh) }
+    AccountsListScreen(vm)
 }
